5ce0707d5e75571e813d46c62af380fc5ecd84d3
Vani-1107 <vaniguptaa2003@gmail.com>
2024-04-11 17:25:57 +0530

    updated

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,7 +2,7 @@ const blogs = require("../models/blogs");
 
 const addBlog = async (req, res) => {
     try {
-        const { mainHeader, header, body, image, link } = req.body;
+        const { header, body, image, link } = req.body;
 
         const newBlog = await blogs.create({
             header,
@@ -43,11 +43,71 @@ const getBlog = async (req, res) => {
     }
 };
 
+const getBlogById = async (req, res) => {
+    try {
+        // Extract the last four digits of the ID from the request parameters
+        const { id } = req.params;
+        // const blog = await blogs.findOne({
+        //     _id: { $regex: new RegExp(`${id}$`, 'i') }
+        // });
+
+
+        const blog1 = await blogs.find();
+
+        // Find the blog with the matching last four characters in its _id
+        const blog = blog1.find(blog => blog._id.toString().endsWith(id));
+
+        if (!blog) {
+            return res.status(404).json({
+                success: false,
+                message: "Blog not found",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: blog,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to get blog",
+            error: err.message,
+        });
+    }
+};
+
 const editBlog = async (req, res) => {
     try {
+        const {id} = req.params;
+        const { header, body, image, link } = req.body;
 
+        // Find the blog by ID and update it
+        const updatedBlog = await blogs.findByIdAndUpdate(id, {
+            header,
+            body,
+            image,
+            link,
+        }, { new: true });
+
+        if (!updatedBlog) {
+            return res.status(404).json({
+                success: false,
+                message: "Blog not found",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Blog updated successfully",
+            data: updatedBlog,
+        });
     } catch (err) {
-
+        res.status(500).json({
+            success: false,
+            message: "Failed to update blog",
+            error: err.message,
+        });
     }
 };
 
@@ -81,4 +141,4 @@ const deleteBlog = async (req, res) => {
     }
 };
 
-module.exports = { addBlog, editBlog, getBlog, deleteBlog };
+module.exports = { addBlog, editBlog, getBlog, deleteBlog, getBlogById };
--- routes/blogs.js
+++ routes/blogs.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router =  express.Router();
 
-const {addBlog, editBlog, getBlog,deleteBlog } = require('../controllers/blogs');
+const {addBlog, editBlog, getBlog,deleteBlog, getBlogById } = require('../controllers/blogs');
 
 router.post("/addBlog" ,addBlog );
 router.get("/getBlogs",getBlog);
 router.put('/editBlog/:id', editBlog);
 router.delete('/deleteBlog/:id',deleteBlog);
+router.get('/getBlogById/:id',getBlogById);
 
 module.exports = router;
